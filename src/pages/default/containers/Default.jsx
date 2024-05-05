e40684b174e07c88e972aca9c78792a8778357f0
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-05-05 22:04:45 +0300

    Added country list and demonstrated on default page

--- src/pages/default/containers/Default.jsx
+++ src/pages/default/containers/Default.jsx
@@ -1,13 +1,21 @@
 import { useIntl } from 'react-intl';
 import React from 'react';
 import Typography from 'components/Typography';
+import { countries } from 'app/data/countries';
 
 function Default() {
   const { formatMessage } = useIntl();
 
+  const countryList = countries.map(country=><tr key={country.id}>
+    <td>{country.name}</td><td>{country.capital}</td><td>{country.population}</td><td>{country.square}</td>
+    </tr >);
+
   return (
     <Typography>
-      {formatMessage({ id: 'title' })}
+      <table>
+        <thead><td>Name</td><td>Capital</td><td>Population</td><td>Total area</td></thead>
+        <tbody> {countryList}</tbody>
+       </table>
     </Typography>
   );
 }
