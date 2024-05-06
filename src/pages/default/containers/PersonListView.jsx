ad2be272033d865a744a83d5462adde2a267d733
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-05-06 18:54:07 +0300

    Person list view implemented as table component with pagination support

--- src/pages/default/containers/PersonListView.jsx
+++ src/pages/default/containers/PersonListView.jsx
@@ -1,34 +1,52 @@
 import React from 'react';
 import Typography from 'components/Typography';
 import { persons } from 'app/data/persons';
-import { List, ListItem, ListItemText, ListItemButton } from '@mui/material';
-import Box from '@mui/material/Box';
-import { FixedSizeList} from 'react-window';
-
-function renderRow(props){
-  const { index, style } = props;
-  return (
-    <ListItem style={style} key={persons[index].id} component="div" disablePadding>
-      <ListItemButton>
-        <ListItemText primary={`${persons[index].name}`} />
-        <ListItemText primary={`${persons[index].birthday}`} />
-        <ListItemText primary={`${persons[index].sex}`} />
-        <ListItemText primary={`${persons[index].countryOfOrigin.name}`} />
-        <ListItemText primary={`${persons[index].citizenship.name}`} />
-      </ListItemButton>
-    </ListItem>
-  );
-
-}
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
 
 function PersonListView() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   return (
     <Typography>
-      <Box sx={{ width: '100%', height: '100%', maxWidth: 720, bgcolor: 'background.paper' }}>
-        <FixedSizeList height={600} width={720} itemSize={30} itemCount={persons.length}>
-            {renderRow}
-        </FixedSizeList>
-      </Box>
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <TableContainer sx={{ maxHeight: 600 }}>
+          <Table sx={{ minWidth: 650 }} size="small" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell align="center" style={{ minWidth: 200 }}>Name</TableCell>
+                <TableCell align="center" style={{ minWidth: 50 }}>Birthday</TableCell>
+                <TableCell align="left" style={{ minWidth: 30 }}>Sex</TableCell>
+                <TableCell align="left" style={{ minWidth: 50 }}>Country of origin</TableCell>
+                <TableCell align="left" style={{ minWidth: 50 }}>Country of citizenship</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                persons.
+                  slice(page * rowsPerPage, (page + 1) * rowsPerPage).
+                  map(person => (
+                    <TableRow key={person.id} hover role="checkbox" tabIndex={-1}>
+                      <TableCell>{person.name}</TableCell>
+                      <TableCell>{person.birthday}</TableCell>
+                      <TableCell>{person.sex === 'MALE' ? 'Male' : 'Female'}</TableCell>
+                      <TableCell>{person.countryOfOrigin.name}</TableCell>
+                      <TableCell>{person.citizenship.name}</TableCell>
+                    </TableRow>
+                  ))
+              }
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination rowsPerPageOptions={[5, 10, 15, 20, 50]} component="div" count={persons.length} rowsPerPage={rowsPerPage} page={page} onPageChange={(event, newPage) => setPage(newPage)} onRowsPerPageChange={(event) => { setRowsPerPage(event.target.value); setPage(0); }} />
+      </Paper>
     </Typography>
   );
 }
